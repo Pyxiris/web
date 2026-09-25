8cf9f51b52165c1c4239b1082f1ef0dc8d83e888
Liam Noonan <ljmnoonan@gmail.com>
2026-09-25 08:13:51 +0000

    [IMP] web_responsive: Add popout button to file viewer
    Not a super important feature, but I have a wide screen and like to pop
    out new windows for files sometimes. Native odoo makes that inconvenient
    (except for vendor bills) so now I have made it convenient.

--- web_responsive/static/src/core/file_viewer/file_viewer.esm.js
+++ web_responsive/static/src/core/file_viewer/file_viewer.esm.js
@@ -7,14 +7,28 @@ import {onMounted, onWillStart, onWillUnmount, useRef} from "@odoo/owl";
 import {FileViewer} from "@web/core/file_viewer/file_viewer";
 import {_t} from "@web/core/l10n/translation";
 import {patch} from "@web/core/utils/patch";
+import {useService} from "@web/core/utils/hooks";
 
 const formChatterAsideSelector = ".o-mail-Form-chatter.o-aside";
+const POPOUT_MANAGER_ID = "web_responsive.file_viewer";
+
+FileViewer.props.push("isPopout?");
+
+/** One manager for the file-viewer popout (createManager resets state if called again). */
+let fileViewerPopoutManager = null;
+
+function getFileViewerPopoutManager(mailPopoutService) {
+    if (!fileViewerPopoutManager) {
+        fileViewerPopoutManager = mailPopoutService.createManager(POPOUT_MANAGER_ID);
+    }
+    return fileViewerPopoutManager;
+}
 
 /** Set minimized height from the aside chatter; width comes from FormChatter. */
 function useFileViewerAsideHeight(ref) {
     let resizeObserver = null;
     function update() {
-        const chatter = document.querySelector(formChatterAsideSelector);
+        const chatter = ref.el?.ownerDocument.querySelector(formChatterAsideSelector);
         if (chatter && ref.el) {
             ref.el.style.setProperty(
                 "--o-FileViewerContainer-height",
@@ -23,7 +37,7 @@ function useFileViewerAsideHeight(ref) {
         }
     }
     onMounted(() => {
-        const chatter = document.querySelector(formChatterAsideSelector);
+        const chatter = ref.el?.ownerDocument.querySelector(formChatterAsideSelector);
         if (!chatter) {
             return;
         }
@@ -38,6 +52,7 @@ patch(FileViewer.prototype, {
     setup() {
         super.setup();
         this.root = useRef("root");
+        this.mailPopoutService = useService("mail.popout");
         Object.assign(this.state, {
             allowMinimize: false,
             maximized: true,
@@ -51,13 +66,27 @@ patch(FileViewer.prototype, {
     },
 
     setDefaultMaximizeState() {
-        this.state.allowMinimize = Boolean(
-            document.querySelector(formChatterAsideSelector)
-        );
+        this.state.allowMinimize =
+            !this.props.isPopout &&
+            Boolean(document.querySelector(formChatterAsideSelector));
         this.state.maximized = !this.state.allowMinimize;
     },
 
     toggleMaximized() {
         this.state.maximized = !this.state.maximized;
+    },
+
+    /**
+     * Open this viewer in a popup (same files + next/prev) and close the main one.
+     */
+    popout() {
+        const manager = getFileViewerPopoutManager(this.mailPopoutService);
+        manager.popout(FileViewer, {
+            files: this.props.files,
+            startIndex: this.state.index,
+            modal: true,
+            isPopout: true,
+        });
+        this.close();
     },
 });
